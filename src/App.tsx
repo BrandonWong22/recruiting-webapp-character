d4a0c02c5b1fb92d1cde83a9815a014344fdc0c5
BrandonWong22 <brandonwong2608@gmail.com>
2025-01-12 12:40:26 -0500

    complete first requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
 import { useState } from 'react';
 import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts'; // i had to edit the import to make it work for me
 
 function App() {
-  const [num, setNum] = useState<number>(0);
+  const [attributes, setAttributes] = useState({
+    Strength: 0,
+    Dexterity: 0,
+    Constitution: 0,
+    Intelligence: 0,
+    Wisdom: 0,
+    Charisma: 0,
+  });
+
+  const incrementAttribute = (attribute: string) => {
+    setAttributes((prev) => ({
+      ...prev,
+      [attribute]: prev[attribute] + 1
+    }));
+  }
+
+  const decrementAttribute = (attribute: string) => {
+    setAttributes((prev) => ({
+      ...prev,
+      [attribute]: prev[attribute] - 1
+    }));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
       <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
+        <div className='attribute-controls'>
+          {ATTRIBUTE_LIST.map((attribute) => {
+            return (
+              <div key={attribute}>
+                <strong style={{marginRight: "8px"}}>{attribute}: {attributes[attribute]}</strong>
+                <button onClick={() => incrementAttribute(attribute)}>+</button>
+                <button onClick={() => decrementAttribute(attribute)}>-</button>
+              </div>
+            )
+          })}
         </div>
       </section>
     </div>
